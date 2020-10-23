6005d3ada3adad0994e5e1695ff300337ba0149d
kangbeom.kim <kangbeom.kim@slexn.com>
2020-10-23 11:14:51 +0900

    defaultProps 를 사용해 onToggle 을 지정하는걸 깜빡했다면 defaultProps 에서 경고로 알려줌.

--- src/User.js
+++ src/User.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-function User ({ user }) {
+function User ({ user, onToggle }) {
     if (!user) return null;
     return (
         <ul>
             {user.map(user => (
-                <li key={user.id}>{user.username}</li>
+                <li key={user.id} onClick={()=> onToggle(user.id)}>
+                    {user.username}
+                </li>
             ))}
         </ul>
     )
 }
 
+User.defaultProps = {
+    onToggle: () => {
+        console.warn('you missed onToggle!');
+    }
+}
 export default User;
