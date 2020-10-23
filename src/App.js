8fd51ac4fe133e12657947200848675f4234fb42
kangbeom.kim <kangbeom.kim@slexn.com>
2020-10-23 11:24:34 +0900

    componentDidCatch 를 사용한 에러 컴포넌트 렌더링

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import User from './User';
-
+import ErrorBoundary from './ErrorBoundary';
 function App() {
   const user = {
     id: 1,
     username: 'HandsomeChoco'
   }
   return (
-    <User/>
+    <ErrorBoundary>
+      <User/>
+    </ErrorBoundary>
   );
 }
 
