c622c1fb5bfefe87ca8e0dc12810b51fed659dae
kangbeom.kim <kangbeom.kim@slexn.com>
2020-10-23 11:53:09 +0900

    배포 환경에서도 Sentry로 에러 전송을 위한 작업

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
+import * as Sentry from '@sentry/browser';
 
+/*
+    프로젝트 완료 후 배포 시, componentDidCatch 로 이미 에러를 잡은 경우, 
+    Sentry 로 에러가 자동으로 전달되지 않는다.
+
+    이 경우 아래와 같이 세팅한다.
+    
+    componentDidCatch 에서 process.env.NODE_ENV 값을 조회해
+    현재 개발환경인지 배포환경인지 확인할 수 있다.
+    개발 환경에서는 captureException 을 사용할 필요가 없기 때문에 아래와 같이 설정한다.
+*/
 class ErrorBoundary extends Component {
     state = {
         error: false
@@ -13,6 +24,9 @@ class ErrorBoundary extends Component {
         this.setState({
             error: true
         });
+        if(process.env.NODE_ENV === 'production') {
+            Sentry.captureException(error, { extra: info });
+        }
     }
     render() {
         if(this.state.error) {
