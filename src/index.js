83750ff1b2100b64b11c9ff8efb3501553447071
kangbeom.kim <kangbeom.kim@slexn.com>
2020-10-23 11:45:25 +0900

    Sentry 를 연결, 에러 발생시 에러를 자동으로 전송

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn: "https://093241b877c34d2a9ff79477c06d4e90@o465895.ingest.sentry.io/5479424",
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
